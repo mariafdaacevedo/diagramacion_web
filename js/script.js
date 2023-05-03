739fd7f4ce1713ed00bc2a874cef2fb3fabe2b8f
Maria Fernanda Acevedo <113567795+mariafdaacevedo@users.noreply.github.com>
2023-05-03 07:42:55 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ addEventListener("DOMContentLoaded", (event) => {
     console.log("DOM fully loaded and parsed")
     /*---------- */
     const divs = document.querySelectorAll('.image-container div');
-const lightbox = document.querySelector('.lightbox');
-const lightboxImage = document.querySelector('.lightbox img');
 
 divs.forEach((div) => {
   div.addEventListener('click', () => {
